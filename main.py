c1e148d371f5706de68321910aae71d024b43454
Pavlo Hilei <pavlikhiley@gmial.com>
2019-11-15 11:31:32 +0200

    updated

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('ok')
+import telebot
+from telebot import types
+
+
+bot = telebot.TeleBot("1055491905:AAE7lgq_WIPI_wPZzbdJluf29Tc1sX024LY")
+@bot.message_handler(content_types = ['text'])
+def send_echo(message):
+    markup = types.ReplyKeyboardMarkup()
+    itembtna = types.KeyboardButton('Додати івент')
+    itembtnv = types.KeyboardButton('Подивитися івенти')
+    markup.row(itembtna, itembtnv)
+    if message.text == '/start':
+        bot.send_message(message.chat.id, "Choose", reply_markup=markup)
+    if message.text == 'Додати івент':
+        pass
+    if message.text == 'Подивитися івенти':
+        pass
+    
+    
+bot.polling( none_stop = True )
